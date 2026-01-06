ef07b19463d0ab6c51364cb8002d14d329247b62
RazvanAndrei200526 <doruippu@gmail.com>
2026-01-07 00:25:29 +0200

    update script for prod

--- apps/paladin/shared/database/data-source.ts
+++ apps/paladin/shared/database/data-source.ts
@@ -1,7 +1,14 @@
-import { getDataSourceForCLI } from '@razvan11/paladin';
-import { PrimaryDatabase } from './PrimaryDatabase';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { PrimaryEntities } from './Entities';
+import { Initial1766947724851 } from '../../../../migrations/1766947724851-Initial';
 
-export const AppDataSource = getDataSourceForCLI(
-  PrimaryDatabase,
-  Bun.env.APP_DATABASE_URL || '',
-);
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.APP_DATABASE_URL || Bun.env.APP_DATABASE_URL || '',
+  synchronize: false,
+  logging: true,
+  entities: PrimaryEntities,
+  migrations: [Initial1766947724851],
+  migrationsTableName: 'migrations',
+});
